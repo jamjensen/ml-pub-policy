8f0e4a03fb4dd003c4ba3b78c94f25b9bff5b917
James Jensen <jamesronaldjensen@gmail.com>
2019-05-01 12:55:45 -0500

    hw3 first pass

---
+++ hw3/explore.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import joypy
+import missingno as msno
+import pipeline
+
+
+COL_NAMES = ['NumberOfTimes90DaysLate', 'MonthlyIncome', 'age']
+LOC = 'zipcode'
+TARGET = 'SeriousDlqin2yrs'
+
+
+def summarize(df, cols):
+	'''
+	Dispalys the moments and quartile ranges for a given
+	list of varables
+	Inputs:
+		df: dataframe
+		cols: list of columns
+	'''
+
+	for col in cols:
+		print(df[col].describe())
+
+
+def target_loc_dist(df, target, loc):
+	'''
+	Percentage of people who have had deliquency
+	Inputs:
+		target (str): name of target column
+		loc (str): name of column with record location
+	'''
+
+	one = df[df[target] == 1].groupby(loc).size()
+	two = df.groupby(loc).PersonID.nunique()
+
+	return one / two
+
+
+def plot_distribution(col):
+	'''
+	Takes the column (str) of a dataframe and plots distribution of its values.
+	Continous variables. 
+	'''
+
+	return sns.distplot(col, hist=False, rug=True);
+
+
+def make_heatmap(df):
+	'''
+	Takes a dataframe and makes a heatmap visualizing correlations among variables
+	'''
+
+	corr = df.corr()
+	ax = sns.heatmap(corr, xticklabels=corr.columns.values, 
+                yticklabels=corr.columns.values)
+
+	return ax
+
+
+def missing_bar(df):
+	'''
+	Visualize total counts of missing values
+	'''
+
+	ax = msno.bar(df, figsize=[9.,7.])
+
+	return ax
+
+
+def missing_corr(df):
+	'''
+	Find correlations among missing data
+	'''
+
+	ax = msno.heatmap(df) 
+
+	return ax
+
+def missing_matrix(df):
+	'''
+	Visualize variation in occurence by index of missing values
+	'''
+
+	ax = msno.matrix(df, figsize=[9.,7.])
+
+	return ax
+
+def null_cols(df):
+
+	isnull = df.isnull().any()
+	null_cols = list(isnull[isnull == True].index)
+
+	return null_cols
+
---
+++ hw3/pipeline.py
@@ -0,0 +1,150 @@
+import pandas as pd 
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+
+TEST_SIZE = 0.3
+RANDOM_STATE = 1
+DISCR_COL = 'MonthlyIncome'
+BIN_LEN = 10000
+
+KEEP_COLS = ['zipcode', 'MonthlyIncome_discrete', 'NumberRealEstateLoansOrLines']
+TARGET_COL = ['SeriousDlqin2yrs']
+
+path = 'data/projects_2012_2013.csv'
+
+class process:
+    '''
+    Class for data pipeline
+    '''
+    def __init__(self, path):
+
+        self.data = self.load_data(path)
+        self.cols = KEEP_COLS
+        self.target = TARGET_COL
+        self.discr_col = DISCR_COL
+        self.y = None
+        self.x = None
+        # self.x = self.set_x()
+        # self.y = self.set_y()
+        # self.df = self.format_df()
+
+
+    def load_data(self, path):
+        '''
+        The data pipeline
+        Inputs:
+            filename (str): name of csv
+        Outputs:
+            df: a dataframe
+        '''
+
+        if os.path.exists(path):
+            df = pd.read_csv(path)
+        else:
+            raise Exception('The file does not exist')
+        
+        return df
+
+
+def fill_continuous_null(df, cols):
+    '''
+    Fills null columns in a dataframe in place
+    Inputs:
+        cols: list of column names corresponding to continuous variables
+            with null values
+    '''
+
+    for col in cols:
+        df[col].fillna(df[col].median(), inplace=True)
+
+    return None
+
+
+def discretize(self, colname, bin_len):
+    '''
+    Discretizes a continuous variable
+    Inputs:
+        df: a dataframe
+        colname (str): name of continuous variable
+        bin_len (int): size of bins 
+    '''
+    
+    lb = self.data[colname].min()
+    ub = self.data[colname].max()
+    bins = np.linspace(lb, ub, bin_len)
+
+    df[colname +'_discrete'] = df[colname].apply(lambda x: np.digitize(x, bins))
+
+    return None
+
+
+def make_binary(self, cols):
+    '''
+    Makes a categorical variable binary
+    Inputs:
+        cols: list of column names
+    '''
+
+    df = pd.get_dummies(self.data, columns=cols)
+
+    return df
+
+
+def set_x(self):
+    '''
+    Creates predictor and target dataframes.
+    Input:
+        df (pandas df): containing predictor and target columns
+        target_col (str): name of target column (predicting)
+    Output:
+        Returns a tuple of dataframes
+    '''
+    
+    self.x = self.data.drop(self.target, axis=1)
+
+    return None
+
+
+def set_y(self):
+    '''
+    Creates predictor and target dataframes.
+    Input:
+        df (pandas df): containing predictor and target columns
+        target_col (str): name of target column (predicting)
+    Output:
+        Returns a tuple of dataframes
+    '''
+    
+    self.y = self.data.loc[:, target_col]
+
+    return None
+
+
+def format_df(self):
+    '''
+    Transforms feature columns into dummy variables and returns
+        dataframe used to build the decision tree
+    '''
+    df = self.data.loc[:, self.cols + self.target]
+    df2 = df.copy()
+
+    return self.make_binary(df2, keep_cols)
+
+
+def get_test_train(self):
+    '''
+    Splits data using scikit test_train
+    Input:
+        x: pandas df of predictor data
+        y: pandas df of target data
+    Output:
+        x_train (df): dataframe
+        x_test (df): dataframe
+        y_train (df): dataframe
+        y_test (df): dataframe
+    '''
+    x_train, x_test, y_train, y_test = train_test_split(self.x, self.y,
+        test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+    return x_train, x_test, y_train, y_test
